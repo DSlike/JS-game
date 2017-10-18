aa5aa04376994b04c06b05cbbcc543d249a87a43
Daniel <daniel.votintsev@gmail.com>
2017-10-18 22:48:14 +0300

    Start rewriting.

--- js/main.js
+++ js/main.js
@@ -1,32 +1,35 @@
+const jumpSize = 40,
+      jumpStep = 10,
+      stepSize = 2,
+      level = 1;
+
 let canvas = document.getElementById("myCanvas"),
     ctx = canvas.getContext("2d"),
     x = 0,
     characterX = 0,
-    characterY = canvas.height - 80,
+    characterY = (canvas.height-40-jumpSize/2)*level,
     characterDefY = characterY,
     rightPressed = false,
     leftPressed = false,
     jump = false;
 
-const jumpSize = 40,
-      jumpStep = 10,
-      stepSize = 2;
-
 canvas.width = window.innerWidth;
 
-let map = [60, 60, 80, 80,80,60];
+let objectsMap = [[0,(canvas.height-40)*level, 40]];
+// let map = [60, 60, 80, 80,80,60];
 
-for(let i=2; i<canvas.width/20; i+=2){
-  let a = Math.floor(Math.random() * (10 - 0)) + 0;
-  if(a > 5){
-    map[i]=80;
-    map[i+1]=80;
+for(let i=0; i<canvas.width/20; i++){
+  let a = Math.floor(Math.random() * (20 - 0)) + 0;
+  if(a<10){
+    objectsMap.push({x: i*20, y:(canvas.height-40)*level, h:40, w:20 });
   }
-  else{
-    map[i]=60;
-    map[i+1]=60;
+  else if(a>=10){
+    objectsMap.push({x: i*20, y:(canvas.height-60)*level, h:60, w:20 });
   }
 }
+
+objectsMap[0].y = canvas.height-40;
+objectsMap[0].h = 40;
 
 function drawCharacter(callback){
   ctx.beginPath();
@@ -39,8 +42,9 @@ function drawCharacter(callback){
 
 function drawMap(){
   ctx.beginPath();
-  for(let i=0; i<map.length; i++){
-    ctx.rect(i*20, canvas.height-map[i], 20, map[i]);
+  for(let i=0; i<objectsMap.length; i++){
+    let o = objectsMap[i];
+    ctx.rect(o.x, o.y, o.w, o.h);
     ctx.fillStyle = "black";
     ctx.lineWidth="0";
     ctx.strokeStyle="white";
@@ -50,53 +54,44 @@ function drawMap(){
   ctx.closePath();
 }
 
-document.addEventListener("keydown", keyDownHandler, false);
-document.addEventListener("keyup", keyUpHandler, false);
-
-function keyDownHandler(e) {
-    if(e.keyCode == 39) {
-        rightPressed = true;
-    }
-    else if(e.keyCode == 37) {
-        leftPressed = true;
-    }
-    else if(e.keyCode == 38 && jump==false) {
-        jump = true;
-        characterDefY = characterY;
-    }
-}
-function keyUpHandler(e) {
-    if(e.keyCode == 39) {
-        rightPressed = false;
-    }
-    else if(e.keyCode == 37) {
-        leftPressed = false;
-    }
-}
-
-function collisionDetection(){
-  let x = Math.round(characterX / 20);
-  let y = characterY-120;
-  if(canvas.height-map[x] == characterDefY && characterX-x*20 < 0){
-    characterX -= Math.abs(characterX-x*20);
-  }
-  else if(canvas.height-map[x] == characterDefY && characterX-x*20 > 0){
-    characterX -= Math.abs(characterX-x*20);
-  }
-
-  if(characterY >= canvas.height-map[x]-30 && characterDefY>canvas.height-map[x]-30){
-    characterDefY = canvas.height-map[x]-30;
-  }
-  else if(characterDefY <= canvas.height-map[x]-20 && jump==false){
-    characterDefY = canvas.height-map[x]-20;
-  }
-}
+// document.addEventListener("keydown", keyDownHandler, false);
+// document.addEventListener("keyup", keyUpHandler, false);
+//
+// function keyDownHandler(e) {
+//     if(e.keyCode == 39) {
+//         rightPressed = true;
+//     }
+//     else if(e.keyCode == 37) {
+//         leftPressed = true;
+//     }
+//     else if(e.keyCode == 38 && jump==false) {
+//         jump = true;
+//         characterDefY = characterY;
+//     }
+// }
+// function keyUpHandler(e) {
+//     if(e.keyCode == 39) {
+//         rightPressed = false;
+//     }
+//     else if(e.keyCode == 37) {
+//         leftPressed = false;
+//     }
+// }
+//
+// function collisionDetection(){
+//   for(let i=0; i<objectsMap.length; i++){
+//     var o = objectsMap[i];
+//     if(characterX>o.x && characterX<o.x+20){
+//       characterX = o.x-20;
+//     }
+//   }
+// }
 
 function draw(){
   ctx.clearRect(0, 0, canvas.width, canvas.height);
   drawCharacter();
   drawMap();
-  collisionDetection();
+  // collisionDetection();
 
   if(characterY<characterDefY){
     characterY+=5;
@@ -107,7 +102,6 @@ function draw(){
   else if(leftPressed && characterX > 0) {
       characterX -= stepSize;
   }
-  collisionDetection();
   if(jump == true && characterY > characterDefY-jumpSize){
     characterY-=jumpStep;
 
@@ -117,7 +111,6 @@ function draw(){
   if(jump == false && characterY < characterDefY){
     characterY+=jumpStep;
   }
-  collisionDetection();
   requestAnimationFrame(draw);
 }
 
